99455d6f9f33ace939c0de27a3d3e1f49f50c05c
Berrybunny015 <berrybunny015@gmail.com>
2021-09-08 11:24:45 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("\nWhat is your age? "); //ask the user's age
+        Scanner userAgeInput = new Scanner(System.in); //scan for the user's age
+        int userAge = userAgeInput.nextInt();
+
+        //determine whether the user is old enough to legally drive or not
+        String legallyDriveResult = (userAge >= 16) ? "\nYou are old enough to legally drive." : "\nYou are not old enough to legally drive.";
+
+        System.out.println(legallyDriveResult); //notify whether the user is able to legally drive or not
+
+    }
+}
